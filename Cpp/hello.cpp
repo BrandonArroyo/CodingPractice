32dcf64f817c47bfe7a115aac0f2059ff8d8e145
BrandonArroyo <barroyo50@gmail.com>
2015-04-06 10:53:54 -0500

    running hello.cpp

--- Cpp/hello.cpp
+++ Cpp/hello.cpp
@@ -1,9 +1,10 @@
+#include <iostream>
 int main(){
-	std:: cout<<"Hello";
-	std:: cout<<"test";
-	std:: cout<<"test";
-	std:: cout<<"test";
-	std:: cout<<"test";
+	std:: cout<<"Hello"<<std::endl;
+	std:: cout<<"test"<<std::endl;
+	std:: cout<<"test"<<std::endl;
+	std:: cout<<"test"<<std::endl;
+	std:: cout<<"test"<<std::endl;
 	return 0;
 
 }
