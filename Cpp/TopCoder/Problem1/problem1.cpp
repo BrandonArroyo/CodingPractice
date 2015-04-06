368c41ff92e2d4dced11cc7e16a5d3c01b9f64b8
BrandonArroyo <barroyo50@gmail.com>
2015-04-06 13:42:54 -0500

    Added problem 1

---
+++ Cpp/TopCoder/Problem1/problem1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+	cout<<"hello"<<endl;
+	return 0;
+}
+class AB {
+
+	string createString(int N, int k){
+		string n_string;
+		while()
+		return n_string;
+	}
+};	
