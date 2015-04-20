1b6e867b66c538d13a1cce265c486bc26bf2d63e
BrandonArroyo <barroyo50@gmail.com>
2015-04-20 01:02:00 -0500

    basic practice in the language

--- Python/hello.py
+++ Python/hello.py
@@ -1,1 +1,23 @@
-print "Hello Devan."
+print "Hello."
+x = 2
+# this is a if statement
+if x ==1:
+	print "x == 1 "
+else:
+	print "x != 1"
+#for loop 
+words = ['cat', 'dog', 'bird']
+
+for w in words:
+	print w
+
+def fib(n):
+	result = []
+	a, b = 0, 1
+	while a < n:
+		result.append(a)
+		print a, 
+		a, b = b, a + b 
+	return result
+
+fib(100)
