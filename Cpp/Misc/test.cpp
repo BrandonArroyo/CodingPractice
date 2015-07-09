16e5c87a971cc3f02e97bc353689af20036346fe
BrandonArroyo <barroyo50@gmail.com>
2015-07-08 19:34:40 -0500

    playing with bitwise

--- Cpp/Misc/test.cpp
+++ Cpp/Misc/test.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
+int isUniqueChar(string s);
 int main(){
-	 cout<<"Hello"<<endl;
-	
+	string s;
+	s = "abcdefghijklmnopqrstuvwxyz";
+	 if(isUniqueChar(s)) cout<<"yes";
+	 else cout<<"false";
 	return 0;
+
+}
+
+int isUniqueChar(string s){
+
+	int checker = 0;
+	cout <<"first checker"<<checker<<endl;
+	for(int i = 0; i< s.length() ;++i){
+		int val = s[i] - 'a';
+		cout <<"val"<<val<<endl;
+		
+		if((checker & (1 << val)) > 0){
+			cout << "this;"<<(checker & (1 << val))<<endl ;
+			return false;
+
+		}
+		checker |= (1 <<val);
+		int test = (1 <<val);
+		cout<< "what does this do:" << val <<"and"<<test<<endl;
+		cout<< "checker new value:"<< checker<<endl;
+	}
+	return true;
 
 }
