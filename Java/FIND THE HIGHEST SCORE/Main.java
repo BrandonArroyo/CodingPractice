2e5f943dbf4e1ef227ff4653c1034579dcfa33fc
BrandonArroyo <barroyo50@gmail.com>
2015-08-25 16:44:02 -0500

    + solution that doesnt work

--- Java/FIND THE HIGHEST SCORE/Main.java
+++ Java/FIND THE HIGHEST SCORE/Main.java
@@ -1,12 +1,33 @@
 import java.io.*;
-public class  {
+import java.util.Arrays;
+public class  Main{
     public static void main (String[] args) throws IOException {
         File file = new File(args[0]);
         BufferedReader buffer = new BufferedReader(new FileReader(file));
         String line;
         while ((line = buffer.readLine()) != null) {
             line = line.trim();
-            // Process line of input Here
+            String[] l = line.split("\\s+");
+
+            parseLine(l);
+            System.out.println("");
         }
     }
+    public static void parseLine(String[] s){
+        int highest = -1001;// this is lowest in possible input
+        
+        for (int i = 0; i < s.length ;++i ) {
+            if(s[i].equals("|")){
+              //System.out.println(Integer.parseInt(s[i]));
+              System.out.print(highest + " ");
+              highest = -1001;
+            }
+            else if(highest < Integer.parseInt(s[i])){
+                highest =  Integer.parseInt(s[i]);
+            }
+
+        }
+        System.out.print(highest); // case for last set
+    }
+
 }
